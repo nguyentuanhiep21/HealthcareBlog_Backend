de6b7893c553f0692d8241207aaa429b148d79d3
nguyentuanhiep21 <145744239+nguyentuanhiep21@users.noreply.github.com>
2025-12-24 13:00:30 +0700

    update db

---
+++ HealthCareBlog_Backend/Migrations/20251224055940_add_admin_role.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCareBlog_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_admin_role : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_admin",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_admin",
+                table: "AspNetUsers");
+        }
+    }
+}
